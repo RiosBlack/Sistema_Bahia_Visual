50f8f4ba0e7517141571f69dbf36356fc0fd424e
Douglas Rios - Trabalho <glinhas55@hotmail.com>
2023-04-20 15:23:40 -0300

    ...

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import LoginFromComponent from 'src/components/loginForm';
 import Image from 'next/image';
+import logo from 'src/assets/logo.png';
+import background from 'src/assets/backgroundLogin.jpg';
 
 export default function Home() {
     return (
@@ -14,19 +16,21 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className="w-screen h-screen grid md:flex p-2">
-                <div className='w-1/2 grid justify-items-center content-center'>
+                <div className="w-1/2 grid justify-items-center content-center">
                     <Image
-                        src="/assets/logo.png"
-                        width={50}
-                        height={50}
-                        quality={100} alt={'Logo da empresa Bahia Visual'}                    />
+                        src={logo}
+                        width={300}
+                        height={300}
+                        quality={100}
+                        alt={'Logo da empresa Bahia Visual'}
+                    />
                     <h1>Bem vindo ao sistema da Bahia Visual</h1>
                     <h2>
                         Você pode acessar o sistema ao lado com seu email e
                         senha cadastrado.
                     </h2>
                 </div>
-                <div className='flex justify-center items-center w-1/2'>
+                <div className="flex justify-center items-center w-1/2">
                     <LoginFromComponent />
                 </div>
             </main>
