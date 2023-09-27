69b70d4629675aa67f7719ccef2e34c506e5b987
riosBlack <glinhas55@hotmail.com>
2023-09-27 19:24:39 -0300

    Corrigindo docker compose

--- deploy/Terraform/datasource.tf
+++
@@ -1,9 +0,0 @@
-data "aws_ami" "dh_pi_ami" {
-  most_recent = true
-  owners = ["099720109477"]
-  
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-}
