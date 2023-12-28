9eff8101d72d8a5c66d0417ba9878cf19ae74a96
riosBlack <glinhas55@hotmail.com>
2023-12-28 11:30:14 -0300

    Iniciando o dashboard

--- frontend2/app/system/page.tsx
+++ frontend2/app/system/page.tsx
@@ -5,8 +5,8 @@ import logo from 'public/logo.png';
 export default function SystemPage() {
 	return (
 		<div className="flex w-full h-full items-center justify-center">
-			<div className="w-1/2 h-full grid justify-items-center content-center bg-white bg-cover rounded-3xl">
-				<video autoPlay loop muted about="video background" className="h-full relative rounded-xl overflow-hidden" >
+			<div className="w-1/2 h-full grid justify-items-center content-center">
+				<video autoPlay loop muted about="video background" className="h-full relative rounded-3xl overflow-hidden" >
 					<source src="/snowBackground.mp4" type="video/mp4" />
 				</video>
 				<div className="grid absolute top-48 justify-items-center content-center bg-yellow-100 bg-opacity-80 m-2 rounded-xl p-2">
