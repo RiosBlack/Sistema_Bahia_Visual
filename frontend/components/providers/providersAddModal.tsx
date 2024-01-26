5943c5dae451f670ea329fa13b04b74ba23a566e
riosBlack <glinhas55@hotmail.com>
2024-01-26 11:13:40 -0300

    finalisando o providers

--- frontend/components/providers/providersAddModal.tsx
+++ frontend/components/providers/providersAddModal.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+"use Client"
+import { useState, useRef, useCallback } from "react";
 import { CgMathPlus } from "react-icons/cg";
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Avatar, Input } from "@nextui-org/react";
+import Webcam from "react-webcam";
+import { FaCamera } from "react-icons/fa";
 
 export default function ProvidersAddModal() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  const webcamRef = useRef(null);
+  const [imgSrc, setImgSrc] = useState(null)
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot({width: 854, height: 480});
+    setImgSrc(imageSrc);
+    console.log(imageSrc);
+    
+  }, [webcamRef, setImgSrc]);
+
 
   return (
     <>
@@ -15,7 +29,21 @@ export default function ProvidersAddModal() {
               <ModalHeader className="flex flex-col gap-1">Adicionar novo prestador</ModalHeader>
               <ModalBody>
                 <div className="w-full flex justify-center items-center">
-                  <Avatar size="lg" />
+                  {imgSrc === null
+                    ?
+                    <div className="grid justify-items-center content-center space-y-1 relative">
+                      <Webcam
+                        className="rounded-full w-44 h-44 border-2 border-blue-600 flex justify-center items-center"
+                        screenshotFormat="image/png"
+                        ref={webcamRef}
+                        audio={false}
+                      />
+                      <div className="w-1 h-1 bg-red-500 rounded-full absolute top-20"></div>
+                      <Button isIconOnly onPress={capture} color="primary" endContent={<FaCamera />}></Button>
+                    </div>
+                    :
+                    <Avatar className="w-28 h-28 text-large" src={imgSrc || ''} />
+                  }
                 </div>
                 <div>
                   <Input
