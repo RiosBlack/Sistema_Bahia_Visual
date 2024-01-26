7586cdb97446e8aeca0dd187c69bf8ac5d9a3c67
riosBlack <glinhas55@hotmail.com>
2024-01-26 11:52:53 -0300

    ajustandando table

--- frontend/components/dashboardRh/providersTable.tsx
+++ frontend/components/dashboardRh/providersTable.tsx
@@ -1,12 +1,12 @@
 'use client'
-import React from "react";
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, Tooltip, ChipProps, getKeyValue} from "@nextui-org/react";
+import { useCallback, useMemo, useState } from "react";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, Tooltip, ChipProps, getKeyValue, Pagination } from "@nextui-org/react";
 import { LuPencilLine } from "react-icons/lu";
 import { RiNewspaperLine } from "react-icons/ri";
 import { FaEye } from "react-icons/fa";
-import {columns, users} from "../../config/providers/data";
+import { columns, users } from "../../config/providers/data";
 
-const statusColorMap: Record<string, ChipProps["color"]>  = {
+const statusColorMap: Record<string, ChipProps["color"]> = {
   active: "success",
   paused: "danger",
   vacation: "warning",
@@ -15,14 +15,26 @@ const statusColorMap: Record<string, ChipProps["color"]>  = {
 type User = typeof users[0];
 
 export default function ProvidersTable() {
-  const renderCell = React.useCallback((user: User, columnKey: React.Key) => {
+  //pagination
+  const [page, setPage] = useState(1);
+  const rowsPerPage = 4;
+  const pages = Math.ceil(users.length / rowsPerPage);
+
+  const items = useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    return users.slice(start, end);
+  }, [page, users]);
+
+  const renderCell = useCallback((user: User, columnKey: React.Key) => {
     const cellValue = user[columnKey as keyof User];
 
     switch (columnKey) {
       case "name":
         return (
           <User
-            avatarProps={{radius: "lg", src: user.avatar}}
+            avatarProps={{ radius: "lg", src: user.avatar }}
             description={user.cpf}
             name={cellValue}
           >
@@ -46,7 +58,7 @@ export default function ProvidersTable() {
           <div className="relative flex items-center gap-2">
             <Tooltip content="Perfil">
               <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-              <FaEye />
+                <FaEye />
               </span>
             </Tooltip>
             <Tooltip content="Folha de pagamento">
@@ -67,7 +79,24 @@ export default function ProvidersTable() {
   }, []);
 
   return (
-  <Table aria-label="Example table with custom cells">
+    <Table
+      bottomContent={
+        <div className="flex w-full justify-center">
+          <Pagination
+            isCompact
+            showControls
+            showShadow
+            color="secondary"
+            page={page}
+            total={pages}
+            onChange={(page) => setPage(page)}
+          />
+        </div>
+      }
+      classNames={{
+        wrapper: "min-h-[222px]",
+      }}
+    >
       <TableHeader columns={columns}>
         {(column) => (
           <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
@@ -75,7 +104,7 @@ export default function ProvidersTable() {
           </TableColumn>
         )}
       </TableHeader>
-      <TableBody items={users}>
+      <TableBody items={items}>
         {(item) => (
           <TableRow key={item.id}>
             {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
